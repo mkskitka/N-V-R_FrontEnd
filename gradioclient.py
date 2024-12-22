55a58226cea812310eff725095f4c96e1d4efbbc
mkskitka <mkskitka@gmail.com>
2024-12-22 15:51:46 -0500

    updates

--- gradioclient.py
+++ gradioclient.py
@@ -4,43 +4,181 @@ import pathlib
 from PIL import Image
 from io import BytesIO
 import tempfile
+from PIL import Image
+import io 
+import base64
+
+# Define custom CSS to use Lato font
+# custom_css = """
+# @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+
+# body {
+#     font-family: 'Lato', sans-serif;
+# }
+# """
+# custom_css = "footer {visibility: hidden}"
+
+custom_css = """
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+
+body, button, input, select, textarea, h1, h2, h3, h4, h5, h6, p, span, label {
+    font-family: 'Lato', sans-serif !important;
+    font-weight: 400;
+    text-transform: uppercase;
+    background-color: black !important;
+}
+
+/* Bold headings */
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 400;
+    text-transform: uppercase; /* Ensure bold text is also uppercase */
+}
+
+/* Make all divs black */
+div {
+    background-color: black !important;
+    color: white !important; /* Ensure text remains visible */
+    border: 0px solid white !important;
+}
+
+/* Title left-aligned */
+h1 {
+    text-align: left !important; /* Left-align the title */
+}
 
 
+/* Add a white thin border to dropdowns, buttons, and form inputs */
+button {
+    border: 1px solid white !important;
+    background-color: black !important; /* Ensure inputs have black background */
+    color: white !important; /* Ensure text inside is visible */
+
+    border-radius: 4px;
+}
+input {
+    background-color: #111 !important;
+    padding: 10px; /* Increase padding to make the dropdown bigger */
+    border-radius: 4px;
+    font-size: 16px; /* Increase font size */
+    width: 100% !important;
+    position: relative !important;
+}
+
+/* Hide the footer */
+footer {
+    visibility: hidden;
+}
+"""
+
+# Paint Names 
+BLACK_HEX = "Black Hex"
+BLACK_MAT = "Black Mat Paint"
+BLACK = "Black Paint"
+BLUE_PAINT = "Blue Paint"
+CAMO_ONE = "Camo 01"
+CAMO_TWO = "Camo 02"
+CAMO_THREE = "Camo 03"
+RED_PAINT = "Red Paint"
+PEARL_ONE = "Pearl 01"
+PEARL_TWO = "Pearl 02"
+GOLD = "Gold Iridescent Paint"
+RUSTED = "Rusted Paint"
+WHITE_MAT =  "White Mat Paint"
+TIGER = "Tiger"
+RED_IRIDESCENT =  "Red Iridescent Paint"
+
+# Background Names 
+NTNSTY = "1960"
+ANIME = "Anime"
+TUNING = "Tuning"
+UNDERGROUND_GARAGE = "Underground Garage"
+UNDERWATER = "Underwater"
+COMIC = "Comic"
+DREAMSCAPE = "Dreamscape Clouds"
+FILM_NOIR = "Film Noir"
+JUNGLE = "Jungle"
+MIAMI_NIGHTS = "Miami Nights"
+NEON = "Neon"
+POST_APO = "Post Apo"
+
+
+paintNames = {
+    "None": "None",
+    BLACK_HEX: "Black_Hex_Paint",
+    BLACK_MAT: "Black_Mat_Paint",
+    BLACK: "Black_Paint",
+    BLUE_PAINT: "Blue_Paint",
+    CAMO_ONE: "Camo_Paint", 
+    CAMO_TWO: "Camouflage2", 
+    CAMO_THREE: "Camouflage3", 
+    RED_PAINT: "Red_Paint",
+    PEARL_ONE: "Pearl",
+    PEARL_TWO: "Pearl02",
+    GOLD: "Gold_Iridescent_Paint",
+    RUSTED: "Rusted_Paint",
+   WHITE_MAT: "White_Mat_Paint",
+   TIGER: "Tiger",
+   RED_IRIDESCENT: "Red_Iridescent_Paint",
+}
+
+backgroundNames = {
+    "None": "None",
+    NTNSTY: "1960",
+    ANIME: "Anime", 
+    TUNING: "Tuning",
+    UNDERGROUND_GARAGE: "UndergroundGarage",
+    UNDERWATER: "Underwater",
+    COMIC: "Comic",
+    DREAMSCAPE: "Dreamscape_Clouds",
+    FILM_NOIR: "Film_Noir",
+    JUNGLE: "Jungle",
+    MIAMI_NIGHTS: "MiamiNights",
+    NEON:  "Neon",
+    POST_APO: "PostApo",
+}
 
 
 def slugify(s: str) -> str:
     return s.lower().replace(" ", "-").replace(".", "-").replace("/", "-")[:32]
 
-# def predict(···) -> np.ndarray | PIL.Image.Image | str | Path | None
-# 	return value
 
 # Define the function to handle the form and make a POST request
-def send_post_request(PositivePrompt, Seed, Steps, Password):
-    url = "https://n-v-r--n-v-r-endpoint-comfyui-api.modal.run"  # Replace with your actual endpoint
+def send_post_request(Car, Paint, Background, Password ):
+    # Dev
+    # url = "https://n-v-r--n-v-r-dream-car-comfyui-api-dev.modal.run"  # Replace with your actual endpoint
+            # Convert the image (PIL format) to bytes
+    buffered = io.BytesIO()
+    Car.save(buffered, format="PNG")
+    buffered.seek(0)
+
+
+     # Encode the image in Base64
+    image_base64 = base64.b64encode(buffered.read()).decode("utf-8")
+
+    # Deployment 
+    url = "https://n-v-r--n-v-r-dream-car-comfyui-api.modal.run"
     payload = {
-        "prompt": PositivePrompt, 
-        "seed": Seed, 
-        "steps": Steps,
-        "password": Password 
+        "password": Password,
+        "paint": paintNames[Paint], 
+        "background": backgroundNames[Background],
+        "car": image_base64
     }
     
     try:
         response = requests.post(url, json=payload)
-        # print("Response Status Code:", response.status_code)
-        # print("Response Text:", response.text)
+        print("\n\n response code: ", response.status_code)
+        # print("\n\n response text: ", response.text)
         # Process the response from the API
         if response.status_code == 200:
-            
+
             # Get the system's temporary directory
             temp_dir = tempfile.gettempdir()
-
+            # print(response.content)
             print(f"Temporary directory: {temp_dir}")
-            filename = pathlib.Path(temp_dir) / f"{slugify(PositivePrompt)}.png"
+            filename1= Paint+"_"+Background
+            filename = pathlib.Path(temp_dir) / f"{slugify(filename1)}.png"
             filename.write_bytes(response.content)
             print(f"saved to '{filename}'")
-            # print(response.content)
-            # Convert the raw bytes to a PIL Image
-            # image = Image.open(BytesIO(response.content))
 
             # image.save(filename, format="PNG")  # Save explicitly as PNG
             return filename  # Return the PIL Image to Gradio
@@ -52,24 +190,43 @@ def send_post_request(PositivePrompt, Seed, Steps, Password):
         print(f"Failed to connect: {str(e)}")
         return None
 
+# Define a custom theme
+# custom_theme = gr.themes.Base(
+#     primary_hue="gray",  # Primary color
+#     secondary_hue="gray"  # Secondary color
+# )
 # Create the Gradio interface
 interface = gr.Interface(
     fn=send_post_request, 
-    theme="soft",
-    title = "Dream Car",
+    theme="Base",
+    # (primary_hue="red", secondary_hue="pink")
+    title = "N-V-R | DREAM CAR",
     inputs= [
-        gr.Textbox(label="PositivePrompt"), 
-        gr.Number(label="Seed", precision=0, value=1),  # Integer input 1
-        gr.Number(label="Steps", precision=0, value=20),  # Integer input 2
-        gr.Textbox(label="Password", type="password")  # Masked input for password
+        gr.Image(type="pil", label="Car"),
+        gr.Dropdown(
+            ["None", BLACK_HEX, BLACK_MAT, BLACK, BLUE_PAINT, RED_PAINT, CAMO_ONE, CAMO_TWO, CAMO_THREE, PEARL_ONE, PEARL_TWO, GOLD, RUSTED,WHITE_MAT,TIGER,RED_IRIDESCENT], 
+            value="None", multiselect=False, label="Paint", info=""
+        ),
+
+        gr.Dropdown(
+                ["None",NTNSTY, ANIME, TUNING, UNDERGROUND_GARAGE, UNDERWATER, COMIC, DREAMSCAPE, FILM_NOIR, JUNGLE, MIAMI_NIGHTS, NEON, POST_APO], 
+                value="None", multiselect=False, label="Background", info=""
+        ),
+        gr.Textbox(label="Password", type="password"),  # Masked input for password
+        # gr.Button(value="Render")  # Custom submit button
     ],
     # outputs=gr.Image()
     outputs=gr.Image(label="Generated Image", type="filepath", format='png'),
-    css="footer {visibility: hidden}",
+    css=custom_css,  # Apply the custom CSS
     # outputs=gr.File(),
-    
     flagging_mode="never"  # Disable the flag button
 )
+
+# Manually create a custom submit button with "Render"
+# submit_button = gr.Button(value="Render")
+
+# Adding the submit button
+# interface.add_component(submit_button)
 
 # Launch the app
 interface.launch(server_name="0.0.0.0", server_port=8080)
